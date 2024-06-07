ffe04bccac759c2752ee23ef5fe284726f0e5107
BluLike <adribelda@gmail.com>
2024-06-07 22:15:53 +0200

    1.2
    Nivel Tutorial WIP
    Rotacion suave de la camara

--- Assets/Scripts/PixelCameraBehavior.cs
+++ Assets/Scripts/PixelCameraBehavior.cs
@@ -15,18 +15,21 @@ public class PixelCameraBehavior : MonoBehaviour
     public int pixelizationFactor = 1;
 
     //La proporción pixel/unidad
-    public float pixelSize;
+    [HideInInspector] public float pixelSize;
 
     //Para seguir al jugador
     public GameObject follow;
+    public Transform rotationHelper;
 
     //Para mantener fija la distancia de la cámara al jugador
     private Vector3 camDistance;
     //Si este valor cambia, se debe rehacer la red texel
     private Quaternion camRotation;
 
-
-
+    public float rotationSpeed = 5f;
+    [HideInInspector] public bool rotating = false;
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -46,67 +49,98 @@ public class PixelCameraBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        
+
+
+        //Si se cambia la rotación, se rehace la red texel
+        if (rotating == true)
         {
-            //Rotar a la derecha
-            RotateRight();
-        }
-        else if (Input.GetKeyDown(KeyCode.Q))
-        {
-            //Rotar a la izquierda
-            RotateLeft();
+            SmoothRotation();
+
         }
         else
-         {
-            //Si se cambia la rotación, se rehace la red texel
-             if (transform.rotation != camRotation)
-             {
-                 camDistance = transform.position - follow.transform.position;
-                 camRotation = transform.rotation;
-                 TexelGrid();
-             }
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Rotar a la derecha
+                RotateRight();
+                rotating = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                //Rotar a la izquierda
+                RotateLeft();
+                rotating = true;
+            }
+            if (transform.rotation != camRotation)
+            {
+                camDistance = transform.position - follow.transform.position;
+                camRotation = transform.rotation;
+                TexelGrid();
+            }
 
-             //Outputcamera se reestablece a su posición
-             outputCamera.transform.position = transform.position;
+            //Outputcamera se reestablece a su posición
+            outputCamera.transform.position = renderCamera.transform.position;
 
-             // Estas funciones sirven para que la cámara siga al jugador y obtener un delta que le dice a la cámara hacia dónde moverse de su posición inicial
-             Vector3 targetPosition = follow.transform.position + camDistance;
-             Vector3 worldDelta = targetPosition - transform.position;
+            // Estas funciones sirven para que la cámara siga al jugador y obtener un delta que le dice a la cámara hacia dónde moverse de su posición inicial
+            Vector3 targetPosition = follow.transform.position + camDistance;
+            Vector3 worldDelta = targetPosition - transform.position;
 
-             //Esto transforma el delta en las coordenadas del mundo a las coordenadas locales de la cámara (teniendo en cuenta la rotación que tiene)
-             //y lo divide por el tamaño del píxel 
-             Vector3 localDelta = transform.InverseTransformDirection(worldDelta);
-            
-            
+            //Esto transforma el delta en las coordenadas del mundo a las coordenadas locales de la cámara (teniendo en cuenta la rotación que tiene)
+            //y lo divide por el tamaño del píxel 
+            Vector3 localDelta = renderCamera.transform.InverseTransformDirection(worldDelta);
+
+
             //Despues de dividirlo, se aproxima y de esa manera, la cámara se "snapea" a los píxeles 
-            Vector3 snappedPos; 
-             snappedPos.x = Mathf.RoundToInt(localDelta.x / pixelSize) * pixelSize;
-             snappedPos.y = Mathf.RoundToInt(localDelta.y / pixelSize) * pixelSize;
-             snappedPos.z = Mathf.RoundToInt(localDelta.z / pixelSize) * pixelSize;
+            Vector3 snappedPos;
+            snappedPos.x = Mathf.RoundToInt(localDelta.x / pixelSize) * pixelSize;
+            snappedPos.y = Mathf.RoundToInt(localDelta.y / pixelSize) * pixelSize;
+            snappedPos.z = Mathf.RoundToInt(localDelta.z / pixelSize) * pixelSize;
 
-             //Aqui símplemente se aplica el snapeo a los píxeles en el movimiento en los ejes de la cámara
-             transform.position +=
-               transform.right * snappedPos.x
-             + transform.up * snappedPos.y
-             + transform.forward * snappedPos.z;
+            //Aqui símplemente se aplica el snapeo a los píxeles en el movimiento en los ejes de la cámara
+            transform.position +=
+              transform.right * snappedPos.x
+            + transform.up * snappedPos.y
+            + transform.forward * snappedPos.z;
 
             //Se registra la posición final de la cámara
-             Vector3 finalPosition = transform.position;
+            Vector3 finalPosition = transform.position;
 
             //Se resta a la output camera el opuesto del movimiento que hace la rendercamera
-             Vector3 offset = new Vector3(finalPosition.x - targetPosition.x, finalPosition.y - targetPosition.y, 0);
-             outputCamera.transform.position -= offset;
-         }
+            Vector3 offset = new Vector3(finalPosition.x - targetPosition.x, finalPosition.y - targetPosition.y, 0);
+            outputCamera.transform.position -= offset;
+        }
     }
   
+    private void SmoothRotation()
+    {
+         
+        transform.position = Vector3.Lerp(transform.position, targetPosition, rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f &&
+            Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+        {
+
+            transform.position = targetPosition;
+            transform.rotation = targetRotation;
+            rotationHelper.position = transform.position;
+            rotationHelper.rotation = transform.rotation;
+
+            rotating = false;
+        }
+    }
     private void RotateRight()
     {
-       
-        transform.RotateAround(follow.transform.position, follow.transform.up, -45);
+        rotationHelper.RotateAround(follow.transform.position, follow.transform.up, -45);
+        targetPosition = rotationHelper.position;
+        targetRotation = rotationHelper.rotation;
     }
     private void RotateLeft()
     {
-        transform.RotateAround(follow.transform.position, follow.transform.up, 45);
+        rotationHelper.RotateAround(follow.transform.position, follow.transform.up, 45);
+        targetPosition = rotationHelper.position;
+        targetRotation = rotationHelper.rotation;
     }
 
     private void TexelGrid()
--- Assets/Scripts/PixelMovementCharacter.cs
+++ Assets/Scripts/PixelMovementCharacter.cs
@@ -7,12 +7,12 @@ public class PixelMovementCharacter : MonoBehaviour
     public float playerSpeed = 4.0f;
     public PixelCameraBehavior pixelCameraBehavior;
     private Camera renderCamera;
-    public float pixelSize;
-    public Vector3 preSnapPos;
+    private float pixelSize;
+    private Vector3 preSnapPos;
     private CharacterController controller;
-    public Vector3 camLocalcurrentPos;
-    public Vector3 localSnappedPosition;
-    public Vector3 snappedPosition;
+    private Vector3 camLocalcurrentPos;
+    private Vector3 localSnappedPosition;
+    private Vector3 snappedPosition;
     private bool rotating;
     private Animator animator;
     Vector3 velocity;
@@ -34,56 +34,64 @@ public class PixelMovementCharacter : MonoBehaviour
         {
             pixelSize = pixelCameraBehavior.pixelSize;
         }
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if (pixelCameraBehavior.rotating == false)
         {
-            animator.SetBool("running", true);
-            // Obtiene la dirección de la cámara
-            Vector3 camForward = renderCamera.transform.forward;
-            Vector3 camRight = renderCamera.transform.right;
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-            // Ignora la rotación en el eje Y de la cámara
-            camForward.y = 0;
-            camRight.y = 0;
-            camForward = camForward.normalized;
-            camRight = camRight.normalized;
+            if (direction.magnitude >= 0.1f)
+            {
+                animator.SetBool("running", true);
+                // Obtiene la dirección de la cámara
+                Vector3 camForward = renderCamera.transform.forward;
+                Vector3 camRight = renderCamera.transform.right;
 
-            // Transforma la dirección del movimiento a la dirección de la cámara
-            direction = camForward * direction.z + camRight * direction.x;
+                // Ignora la rotación en el eje Y de la cámara
+                camForward.y = 0;
+                camRight.y = 0;
+                camForward = camForward.normalized;
+                camRight = camRight.normalized;
 
-            transform.forward = direction;
-            // Mueve al jugador en la dirección transformada
-            controller.Move(direction * (playerSpeed*pixelSize*100) * Time.deltaTime);
+                // Transforma la dirección del movimiento a la dirección de la cámara
+                direction = camForward * direction.z + camRight * direction.x;
 
-            
-            velocity.y += -gravity * Time.deltaTime;
+                transform.forward = direction;
+                // Mueve al jugador en la dirección transformada
+                controller.Move(direction * (playerSpeed * pixelSize * 100) * Time.deltaTime);
 
-            controller.Move(velocity * Time.deltaTime);
+
+                velocity.y += -gravity * Time.deltaTime;
+
+                controller.Move(velocity * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("running", false);
+            }
+
+
+
+            Vector3 currentPosition = transform.position;
+
+            preSnapPos = currentPosition;
+
+            camLocalcurrentPos = renderCamera.transform.InverseTransformPoint(currentPosition);
+
+
+            float snappedX = Mathf.RoundToInt(camLocalcurrentPos.x / pixelSize) * pixelSize;
+            float snappedY = Mathf.RoundToInt(camLocalcurrentPos.y / pixelSize) * pixelSize;
+            float snappedZ = Mathf.RoundToInt(camLocalcurrentPos.z / pixelSize) * pixelSize;
+
+            localSnappedPosition = new Vector3(snappedX, snappedY, snappedZ);
+
+            snappedPosition = renderCamera.transform.TransformPoint(localSnappedPosition);
+            transform.position = snappedPosition;
         }
         else
         {
             animator.SetBool("running", false);
         }
-
-
-
-        Vector3 currentPosition = transform.position;
-
-            preSnapPos = currentPosition;            
-
-            camLocalcurrentPos = renderCamera.transform.InverseTransformPoint(currentPosition);
-
-            
-            float snappedX = Mathf.RoundToInt(camLocalcurrentPos.x / pixelSize) * pixelSize;
-            float snappedY = Mathf.RoundToInt(camLocalcurrentPos.y / pixelSize) * pixelSize;
-            float snappedZ = Mathf.RoundToInt(camLocalcurrentPos.z / pixelSize) * pixelSize;
-
-            localSnappedPosition = new Vector3(snappedX,snappedY,snappedZ);
-
-            snappedPosition = renderCamera.transform.TransformPoint(localSnappedPosition);
-            transform.position = snappedPosition;
     }
 }
