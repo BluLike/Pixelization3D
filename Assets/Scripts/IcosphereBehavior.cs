fa87c73300e0a0440f5235e1c3fd3ab0836b61e5
BluLike <adribelda@gmail.com>
2024-06-09 21:54:40 +0200

    1.4
    -Segundo Nivel
    -Configuración de Outlines y Edges ingame
    -Transiciones de Nivel

--- Assets/Scripts/IcosphereBehavior.cs
+++ Assets/Scripts/IcosphereBehavior.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class IcosphereBehavior : MonoBehaviour
 {
-    public GameObject Player;
-    private Animator PlayerAnimator;
-
-    public GameObject InteractPrompt;
     public float rotationSpeed = 10f;
     public float amplitude = 0.5f;
     public float frequency = 1f;
@@ -17,8 +13,7 @@ public class IcosphereBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position;
-        PlayerAnimator = Player.GetComponent<Animator>();
+        startPos = transform.position;     
     }
 
     // Update is called once per frame
@@ -30,22 +25,5 @@ public class IcosphereBehavior : MonoBehaviour
         endPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
 
         transform.position = endPos;
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (Input.GetKey(KeyCode.F))
-        {
-            Vector3 lookPos = new Vector3(transform.position.x, 0, transform.position.z);
-            Player.transform.LookAt(lookPos);
-            PlayerAnimator.SetBool("interact", true);
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        InteractPrompt.SetActive(true);
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        InteractPrompt.SetActive(false);
     }
 }
---
+++ Assets/Scripts/PostProcessManagerScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessManagerScript : MonoBehaviour
+{
+    public Material Edge;
+    [Range(0.0001f, 10f)]
+    public float edgeThreshold = 0.5f;
+    public Color edgeColor = Color.white;
+    [Range(0f, 1f)]
+    public float edgeOpacity = 0f;
+    public bool color = false;
+
+    public Material Outline;
+    [Range(0.01f, 10f)]
+    public float outlineThreshold = 0.01f;
+    public Color outlineColor = Color.black;
+    [Range(0f,1f)]
+    public float outlineOpacity = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Edge.SetFloat("_Edge_Threshold", edgeThreshold);
+        Edge.SetColor("_Edge_Color", edgeColor);
+        Edge.SetFloat("_Edge_Opacity", edgeOpacity);
+
+        if (color) Edge.EnableKeyword("_COLOR");
+        else Edge.DisableKeyword("_COLOR");
+
+        Outline.SetFloat("_Outline_Threshold", outlineThreshold);
+        Outline.SetColor("_Outline_Color", outlineColor);
+        Outline.SetFloat("_Outline_Opacity", outlineOpacity);
+    }
+
+    private void OnValidate()
+    {
+        Edge.SetFloat("_Edge_Threshold", edgeThreshold);
+        Edge.SetColor("_Edge_Color", edgeColor);
+        Edge.SetFloat("_Edge_Opacity", edgeOpacity);
+
+        if (color) Edge.EnableKeyword("_COLOR");
+        else Edge.DisableKeyword("_COLOR");
+
+        Outline.SetFloat("_Outline_Threshold", outlineThreshold);
+        Outline.SetColor("_Outline_Color", outlineColor);
+        Outline.SetFloat("_Outline_Opacity", outlineOpacity);
+    }
+}
